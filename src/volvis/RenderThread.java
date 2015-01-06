c40c18a999ecf94b7abe2f9c928175ffadfa66c9
LightChaosman <lightchaosman@hotmail.com>
2015-01-06 12:03:10 +0100

    test

---
+++ src/volvis/RenderThread.java
@@ -0,0 +1,40 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volvis;
+
+import javax.media.opengl.GL2;
+import javax.swing.SwingWorker;
+import volume.Volume;
+
+public class RenderThread extends SwingWorker {
+    
+    private RaycastRenderer renderer;
+    private GL2 gl;
+    
+    
+    private final int resolution; // TODO remove
+    
+    public RenderThread(GL2 gl, int mode, int resolution, boolean trilinint, Volume vol, TransferFunction tFunc) {
+        renderer = new RaycastRenderer(mode, resolution, trilinint, vol, tFunc);
+        this.gl = gl;
+        
+        this.resolution = resolution;
+    }
+
+    @Override
+    public Object doInBackground() {
+        //System.out.println("Starting computation for renderer"+resolution);
+        renderer.visualize(gl);
+        //System.out.println("Done with computation for renderer"+resolution);
+        
+        return null;
+    }
+    
+    public void stop() {
+        //System.out.println("Stopping computation for renderer"+resolution);
+        renderer.stopSlicer();
+    }
+    
+}
